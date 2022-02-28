a378c78a11a38797e37efbedfc97f0dac580264f
Luca Mock <luca.mock@ringieraxelspringer.ch>
2022-02-28 13:51:49 +0100

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+
+import * as remote from "@syncstate/remote-client";
+
+import App from "./App.js";
+import { Provider } from "@syncstate/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createDocStore } from "@syncstate/core";
+import io from "socket.io-client";
+import reportWebVitals from "./reportWebVitals";
+
+const store = createDocStore({ todos: [] }, [remote.createInitializer()]);
+
+//enable remote plugin
+store.dispatch(remote.enableRemote("/todos"));
+
+//setting up socket connection with the server
+let socket = io.connect("http://localhost:8000");
+
+// send request to server to get patches everytime when page reloads
+socket.emit("fetchDoc", "/todos");
+
+//observe the changes in store state
+store.observe(
+  "doc",
+  "/todos",
+  (todos, change) => {
+    if (!change.origin) {
+      //send json patch to the server
+      socket.emit("change", "/todos", change);
+    }
+  },
+  Infinity
+);
+
+//get patches from server and dispatch
+socket.on("change", (path, patch) => {
+  // console.log(patch);
+  store.dispatch(remote.applyRemote(path, patch));
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
